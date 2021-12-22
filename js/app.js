651b1ccd0b6791bc5e092b200fbe1bef8b93792f
girts521 <girts521@gmail.com>
2021-12-22 10:52:15 +0100

    created repo

---
+++ js/app.js
@@ -0,0 +1,43 @@
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
+const nav = document.querySelector('nav');
+const header = document.querySelector('header');
+
+window.addEventListener('load', () => {
+    if (window.innerWidth < 768) {
+        navLinks.setAttribute('id', 'navLinks')
+    } else {
+        navLinks.removeAttribute('id', 'navLinks')
+    }
+});
+
+document.addEventListener('scroll', () => {
+    if (window.innerWidth > 1439){
+        if(window.scrollY > 200){
+            nav.setAttribute('style', 'background-color: rgba(5, 3, 3, 0.4);')
+        }
+        if(window.scrollY < 200){
+            nav.removeAttribute('style', 'background-color: rgba(5, 3, 3, 0.4);')
+        }
+    }
+})
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth < 768) {
+        navLinks.setAttribute('id', 'navLinks')
+    } else {
+        navLinks.removeAttribute('id', 'navLinks');
+        navLinks.removeAttribute('style');
+        nav.removeAttribute('style')
+    }
+})
+
+burger.addEventListener('click', () => {
+    if (navLinks.hasAttribute('style')) {
+        navLinks.removeAttribute('style')
+        nav.removeAttribute('style', 'opacity:1;')
+    } else {
+        navLinks.setAttribute('style', 'display: flex; flex-direction: column;')
+        nav.setAttribute('style', 'background-color: rgba(5, 3, 3, 0.9); transition:0.4s;')
+    }
+})
---
+++ styles/contact_page/app.js
@@ -0,0 +1,70 @@
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
+const nav = document.querySelector('nav');
+const header = document.querySelector('header');
+const btn = document.querySelector('#btn');
+const name = document.querySelector('#name');
+const email = document.querySelector('#email');
+const text = document.querySelector('#text');
+
+//nav functionality
+
+window.addEventListener('load', () => {
+    if (window.innerWidth < 768) {
+        navLinks.setAttribute('id', 'navLinks')
+    } else {
+        navLinks.removeAttribute('id', 'navLinks')
+    }
+});
+
+document.addEventListener('scroll', () => {
+    if (window.innerWidth > 1439){
+        if(window.scrollY > 200){
+            nav.setAttribute('style', 'background-color: rgba(156, 39, 176, 0.4);')
+        }
+        if(window.scrollY < 200){
+            nav.removeAttribute('style', 'background-color: rgba(156, 39, 176, 0.4);')
+        }
+    }
+})
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth < 768) {
+        navLinks.setAttribute('id', 'navLinks')
+    } else {
+        navLinks.removeAttribute('id', 'navLinks')
+    }
+})
+
+burger.addEventListener('click', () => {
+    if (navLinks.hasAttribute('style')) {
+        navLinks.removeAttribute('style')
+        nav.removeAttribute('style', 'opacity:1;')
+    } else {
+        navLinks.setAttribute('style', 'display: flex; flex-direction: column;')
+        nav.setAttribute('style', 'background-color: rgba(156, 39, 176, 0.9); transition:0.4s;')
+    }
+})
+
+//Form submittion
+
+btn.addEventListener('click', (event) => {
+    console.log('click')
+    event.preventDefault()
+    const data = {
+       name: name.value,
+       email: email.value, 
+       text: text.value
+    }
+    console.log(data)
+
+axios.post('http://3.64.61.82:5000/data', {data})
+// axios.post('http://localhost:5000/data', {data})
+.then(res => {
+    console.log(res.data.msg)
+})
+.catch(e => {
+    console.log(e)
+})
+
+})
